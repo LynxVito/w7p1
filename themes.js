4174b41e1b51618e4279299c5e7237ace34765b3
LynxVito <mzb508@york.ac.uk>
2025-03-30 17:42:43 +0100

    working on storage

--- themes.js
+++ themes.js
@@ -1,3 +1,5 @@
+const THEME = "Theme";
+
 function Theme(bgColour, font, textColour, textSize) {
     this.backgroundColour = bgColour;
     this.fontFamily = font;
@@ -16,26 +18,41 @@ function ApplyTheme(selectTheme) {
     document.getElementsByTagName("header")[0].style.fontSize = selectTheme.textSize;
 }
 
-ApplyTheme(theme1);
-
 const radioButtons = document.getElementsByName("radio-button");
-console.log(radioButtons)
+console.log(radioButtons);
 
-function changeTheme(event) {
+function ChangeTheme(event) {
     if(event.target.value === "theme1") {
         ApplyTheme(theme1);
+        saveThemeInStorage(event.target.value);
     }
     else if(event.target.value === "theme2") {
         ApplyTheme(theme2);
+        saveThemeInStorage(event.target.value);
     }
     else if(event.target.value === "theme3") {
         ApplyTheme(theme3);
+        saveThemeInStorage(event.target.value);
     }
 }
 
 for(let themeOption of radioButtons) {
-    themeOption.addEventListener("change", changeTheme);
+    themeOption.addEventListener("change", ChangeTheme);
 }
 
+function getThemeFromStorage() {
+    const selectedTheme = localStorage.getItem(THEME);
+    if(selectedTheme !== null) {
+        return selectedTheme;
+    }
+    else {
+        return "theme1";
+    }
+}
 
+function saveThemeInStorage(theme) {
+    localStorage.setItem(THEME, theme);
+    ChangeTheme();
+}
 
+ApplyTheme(theme1);
